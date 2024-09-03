2d57201e9e59e2bedd7fe6316c484a5b399e66e5
asif331001 <asif95331001@gmail.com>
2024-09-03 12:39:30 +0600

    first commit

---
+++ lib/app/app.dart
@@ -0,0 +1,29 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/material.dart';
+import 'package:google_map_assaignment/features/map/view/map_view.dart';
+
+import 'package:provider/provider.dart';
+
+import '../features/map/view_model/location_view_model.dart';
+
+class GoogleMap extends StatelessWidget {
+  const GoogleMap({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => LocationViewModel(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "Google Map",
+        home: const MapView(),
+        locale: DevicePreview.locale(context),
+        builder: DevicePreview.appBuilder,
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/material.dart';
+
+import 'app/app.dart';
+
+
+void main() {
+  runApp(DevicePreview(builder: (_)=> const GoogleMap()));
+}
